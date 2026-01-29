60b18aa48a183c8bf8357dcabfcf020a0c4c9bba
JACKDANIELS-777 <78420661+JACKDANIELS-777@users.noreply.github.com>
2026-01-29 22:55:30 +0200

    Add HexForm.asm with command strings

---
+++ Exstras/Layout/TrenchScript/V0.009/HexForm.asm
@@ -0,0 +1,9 @@
+TrenchScriptStr: 
+db 43h, 68h, 6Eh, 67h, 43h, 6Fh, 6Dh, 20h ; "ChngCom "
+db 43h, 6Ch, 73h, 20h, 20h, 20h, 20h, 20h ; "Cls     "
+db 43h, 6Ch, 65h, 61h, 72h, 20h, 20h, 20h ; "Clear   "
+db 43h, 68h, 6Eh, 67h, 43h, 6Fh, 6Dh, 20h ; "ChngCom "
+db 43h, 6Ch, 65h, 61h, 72h, 20h, 20h, 20h ; "Clear   "
+db 43h, 20h, 20h, 20h, 20h, 20h, 20h, 20h ; "C       "
+db 43h, 20h, 20h, 20h, 20h, 20h, 20h, 20h ; "C       " 
+db 0
