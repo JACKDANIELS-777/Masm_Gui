06d8554805241446dfc75aaca9155eb30ce27766
JACKDANIELS-777 <78420661+JACKDANIELS-777@users.noreply.github.com>
2026-02-01 15:30:52 +0200

    Create str.asm

---
+++ Exstras/Layout/TrenchScript/V0.024/str.asm
@@ -0,0 +1,10 @@
+ TrenchScriptStr:
+        db "AndR    "
+        db "0       "
+        db "0       "
+        dq -1
+ TrenchScriptStr:
+        db "OrR     "
+        db "0       "
+        db "0       "
+        dq -1
